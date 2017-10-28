91a50a5ddf306e46bf0481183f4ac3ea4aee62ab
Kieran <kirreh.g@gmail.com>
2017-10-28 23:19:40 +0700

    added smango resort to grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -5,7 +5,11 @@
           </div>
           <div class="tile is-ancestor projects">
                <port-card  projectImage="./static/web-development-cambodia.jpeg" title="CRM" siteLink="https://www.responsiblemanagement.org.uk" description="The Centre for Resposonsible Management was developed for the University of Winchester to empower responsible managers & leaders to make a positive impact on the planet."></port-card>
-               <port-card   projectImage="./static/web-development-dog-walking-cambodia.jpeg" title="Happy Schnozzles" siteLink="https://www.happyschnozzles.com/" description="Happy Schnozzles was developed to help a local dog walking company grow their online presence. Since launch the site is receving an average of 500 page views per month with a bounce rate of only 1%."></port-card>
+               <port-card   projectImage="./static/web-development-dog-walking-cambodia.jpeg" title="Happy Schnozzles" siteLink="https://www.happyschnozzles.com/" description="Happy Schnozzles was developed to help a local dog walking company grow their online presence. Since launch the site is receving an average of 500 views per month with a bounce rate of only 1%."></port-card>
+          </div>
+          <div class="tile is-ancestor projects">
+               <port-card   projectImage="./static/freelance-web-developer-phnom-penh.jpeg" title="S-Mango House Resort" siteLink="http://smangohouse-cambodia.com" description="SMango House Resort in Cambodia came to me looking for a complete site re-design &amp build. Using Bootstrap, GruntJS and some PHP magic the result was a clean and speedy site that drives more bookings."></port-card>
+               <port-card   projectImage="./static/web-development-dog-walking-cambodia.jpeg" title="S-Mango House Resort" siteLink="https://www.happyschnozzles.com/" description="Happy Schnozzles was developed to help a local dog walking company grow their online presence. Since launch the site is receving an average of 500 page views per month with a bounce rate of only 1%."></port-card>
           </div>
           <div class="container">
                <h2 class="subtitle">Personal Project</h2>
@@ -14,20 +18,19 @@
                <port-card  projectImage="./static/web-development-cambodia.jpeg" title="Cambodia Weather" description="To help improve my development skills, in particular vuejs, I deceided to take on the challenge of creating a native weather app for Cambodia"></port-card>
                <port-card   projectImage="./static/web-development-dog-walking-cambodia.jpeg" title="Happy Schnozzles" description="Happy Schnozzles was developed to help a local dog walking company grow their online presence. Since launch the site is receving an average of 500 page views per month with a bounce rate of only 1%."></port-card>
           </div>
-          <div class="container">
-               <h2 class="subtitle">My road to we dev</h2>
-               <p>Instagram feed from API here.</p>
-          </div>
+          <playlist></playlist>
      </div>
 
 </template>
 
 <script>
 import Card from './Card.vue'
+import Spotify from './Spotify.vue'
 export default {
      name: 'Grid',
      components: {
-          'port-card': Card
+          'port-card': Card,
+          'playlist': Spotify
      }
 }
 </script>
---
+++ src/components/Spotify.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+     <div class="container">
+          <h2 class="subtitle">My road to Web Devlopment</h2>
+          <p>Instagram feed from API here.</p>
+     </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
